40fa9d793e371db9aad0d8a853f17e08cb59b4f0
David Krieger <davidkrieger@Davids-Air.local.tld>
2019-02-07 12:53:34 -0700

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,5 +1,27 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png",
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage:"images/queen-of-diamonds.png",
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts",
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds",
+}, 
+]
 var cardsInPlay = [];
+
 function checkForMatch (){
 	if (cardsInPlay[0]===cardsInPlay[2]){
 		alert("You found a match!")
@@ -8,13 +30,14 @@ function checkForMatch (){
 	}
 	}
 
-	function flipCard(cardId){
-	console.log("User flipped "+cards[cardId]);
-	cardsInPlay.push[0];
-	}
+function flipCard(cardId){
+	console.log("User flipped "+cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].rank);
 	if (cardsInPlay.length === 2) {
 	checkForMatch();
-	}
+	}}
 
 flipCard(0)
 flipCard(2)
